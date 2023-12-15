70cdff11501adce57bc2e01046bed0e50a1a73c4
shahabas <shezilaman@gmail.com>
2023-12-15 16:23:37 +0530

    updated

---
+++ src/components/Body.jsx
@@ -0,0 +1,16 @@
+export function Body(props){
+    let imageData = props.shabs
+    return(
+        <div>
+        {
+          imageData.map(image=>{
+            return(
+              <div key={image.id} className="column">
+                <img src={image.img} alt=""></img>
+              </div>
+            )
+          })
+        }
+      </div>
+    )
+  }
